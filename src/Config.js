8c99d9e01e3002581becabe05637e9b47923ee39
Ramraghul <raghulraghul111@gmail.com>
2022-11-14 18:57:14 +0530

    API connect

--- src/Config.js
+++ src/Config.js
@@ -1,4 +1,4 @@
 export const API = {
-    User:'http://localhost:7777/User',
-    URL:'http://localhost:7777/Url'
+    User:'https://url-short-backend.vercel.app/User',
+    URL:'https://url-short-backend.vercel.app/Url'
 }
